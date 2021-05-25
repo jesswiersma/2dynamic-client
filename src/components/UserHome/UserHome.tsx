6c715d2b4d6213bc46b0834d5649bd732e7ced11
Jessica Wiersma <jesswiersma@gmail.com>
2021-05-25 15:22:49 -0400

    city sheet

--- src/components/UserHome/UserHome.tsx
+++ src/components/UserHome/UserHome.tsx
@@ -1,6 +1,8 @@
+import { userInfo } from "os";
 import React from "react";
 import UserAnnouncementDisplay from "./UserAnnounce";
 import UserScheduleDisplay from "./UserSchedule";
+import CitySheet1 from "../assets/CitySheet1.png";
 
 const UserHome: React.FunctionComponent = () => {
     return (
@@ -11,6 +13,8 @@ const UserHome: React.FunctionComponent = () => {
         <UserAnnouncementDisplay/>
         <p>Schedule</p>
         <UserScheduleDisplay/> 
+        <p>City Sheets, Housing Info, Etc</p>
+        <img id="citySheet" src={CitySheet1} alt="citySheet1"/>
         </div>
         </div>
     )
